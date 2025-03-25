66a3c7775516d4dbe84491254e43ea07187ddbbe
Jair Junior Da Silva Peixoto <168370348+jjpxt@users.noreply.github.com>
2025-03-24 21:54:17 -0300

    crud kkkk
    Permite excluir, editar e visualizar

--- app.js
+++ app.js
@@ -72,3 +72,61 @@ const adicionarProduto = () => {
     dgv(); // Atualizar tabela
   }
 };
+
+// Função de renderização (mantida do código anterior)
+const dgv = (configdgv = { idDestino: "dgvDados" }) => {
+  inicializarProdutos();
+  const dgvDados = document.getElementById(configdgv.idDestino);
+  dgvDados.innerHTML = "";
+
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
+  produtos.forEach((el) => {
+    const dgvLinha = document.createElement("div");
+    dgvLinha.setAttribute("class", "dgvLinha");
+
+    const c1 = document.createElement("div");
+    c1.setAttribute("class", "coluna c1");
+    c1.innerHTML = el.id;
+    dgvLinha.appendChild(c1);
+
+    const c2 = document.createElement("div");
+    c2.setAttribute("class", "coluna c2");
+    c2.innerHTML = el.produto;
+    dgvLinha.appendChild(c2);
+
+    const c3 = document.createElement("div");
+    c3.setAttribute("class", "coluna c3");
+    c3.innerHTML = el.marca;
+    dgvLinha.appendChild(c3);
+
+    const c4 = document.createElement("div");
+    c4.setAttribute("class", "coluna c4");
+    c4.innerHTML = el.modelo;
+    dgvLinha.appendChild(c4);
+
+    const c5 = document.createElement("div");
+    c5.setAttribute("class", "coluna c5");
+    dgvLinha.appendChild(c5);
+
+    const imgDelete = document.createElement("img");
+    imgDelete.setAttribute("class", "dgvIcone");
+    imgDelete.setAttribute("src", "images/lixeira.svg");
+    imgDelete.addEventListener("click", () => excluirProduto(el.id));
+    c5.appendChild(imgDelete);
+
+    const imgEditar = document.createElement("img");
+    imgEditar.setAttribute("class", "dgvIcone");
+    imgEditar.setAttribute("src", "images/editar.svg");
+    imgEditar.addEventListener("click", () => editarProduto(el));
+    c5.appendChild(imgEditar);
+
+    const imgExibir = document.createElement("img");
+    imgExibir.setAttribute("class", "dgvIcone");
+    imgExibir.setAttribute("src", "images/visualizar.svg");
+    imgExibir.addEventListener("click", () => visualizarProduto(el));
+    c5.appendChild(imgExibir);
+
+    dgvDados.appendChild(dgvLinha);
+  });
+};
